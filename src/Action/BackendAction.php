58367d905a94d4bd445fc2d5a00d47aa8b5e98d8
core23 <mail@core23.de>
2018-05-21 11:08:09 +0200

    Use ADR pattern

---
+++ src/Action/BackendAction.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\ShariffBundle\Action;
+
+use Core23\ShariffBundle\Backend\BackendManager;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+final class BackendAction
+{
+    /**
+     * @var BackendManager
+     */
+    private $backend;
+
+    /**
+     * BackendAction constructor.
+     *
+     * @param BackendManager $backend
+     */
+    public function __construct(BackendManager $backend)
+    {
+        $this->backend = $backend;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        $url    = $request->get('url');
+        $result = $this->backend->get($url);
+
+        return new JsonResponse($result);
+    }
+}
--- src/DependencyInjection/Core23ShariffExtension.php
+++ src/DependencyInjection/Core23ShariffExtension.php
@@ -27,6 +27,7 @@ final class Core23ShariffExtension extends Extension
         $config        = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('action.xml');
         $loader->load('block.xml');
         $loader->load('services.xml');
 
