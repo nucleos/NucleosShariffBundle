c65c3c5875a45118e0033b42e3aacfb9a841186f
core23 <mail@core23.de>
2018-12-24 12:46:26 +0100

    Removed deprecation warnings

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -23,6 +23,7 @@ final class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('core23_shariff');
 
+        /* @var ArrayNodeDefinition $rootNode */
         // Keep compatibility with symfony/config < 4.2
         if (!\method_exists($treeBuilder, 'getRootNode')) {
             $rootNode = $treeBuilder->root('core23_shariff');
