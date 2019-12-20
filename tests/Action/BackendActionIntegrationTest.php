66cd9a1c96f015cde80441f559ff90ac6deb5d3e
core23 <mail@core23.de>
2019-12-20 16:36:49 +0100

    Add integration test

---
+++ tests/Action/BackendActionIntegrationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\ShariffBundle\Tests\Action;
+
+use Core23\ShariffBundle\Tests\App\AppKernel;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpKernel\Client;
+
+final class BackendActionIntegrationTest extends TestCase
+{
+    public function testStartup(): void
+    {
+        if (class_exists(KernelBrowser::class)) {
+            $client = new KernelBrowser(new AppKernel());
+        } else {
+            $client = new Client(new AppKernel());
+        }
+
+        $client->request('GET', '/share');
+
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+    }
+}
