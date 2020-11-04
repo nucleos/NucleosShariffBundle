d0983c3130952bcd9d9e3ca084aff23f8d54d4ce
core23 <mail@core23.de>
2020-11-04 10:15:32 +0100

    Fix tagging services

--- tests/Action/BackendActionIntegrationTest.php
+++ tests/Action/BackendActionIntegrationTest.php
@@ -11,23 +11,23 @@ declare(strict_types=1);
 
 namespace Nucleos\ShariffBundle\Tests\Action;
 
-use Nucleos\ShariffBundle\Tests\App\AppKernel;
-use PHPUnit\Framework\TestCase;
-use Symfony\Bundle\FrameworkBundle\KernelBrowser;
-use Symfony\Component\HttpKernel\Client;
+use Nucleos\ShariffBundle\Manager\ServiceManager;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-final class BackendActionIntegrationTest extends TestCase
+final class BackendActionIntegrationTest extends WebTestCase
 {
-    public function testStartup(): void
+    public function testShare(): void
     {
-        if (class_exists(KernelBrowser::class)) {
-            $client = new KernelBrowser(new AppKernel());
-        } else {
-            $client = new Client(new AppKernel());
-        }
-
+        $client = static::createClient();
         $client->request('GET', '/share');
 
         static::assertSame(200, $client->getResponse()->getStatusCode());
+
+        $container      = static::$kernel->getContainer();
+        $serviceManager = $container->get('test_service_manager');
+
+        \assert($serviceManager instanceof ServiceManager);
+
+        static::assertCount(8, $serviceManager->getAll());
     }
 }
