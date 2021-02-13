ccea1e6294e0adf008ab79b3b69fbab424d898ed
core23 <mail@core23.de>
2021-02-13 15:45:41 +0100

    Remove unnecessary assert

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace Nucleos\ShariffBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -23,8 +22,6 @@ final class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder('nucleos_shariff');
 
         $rootNode = $treeBuilder->getRootNode();
-
-        \assert($rootNode instanceof ArrayNodeDefinition);
 
         $this->addAliases($rootNode);
         $this->addOptions($rootNode);
